569ddb4551bcb81dda4fa6d9fc4d5230a5b31d1f
김태성 <ts4683@gmail.com>
2023-02-24 23:27:27 +0900

    Add files via upload

---
+++ DayMoney/DayMoney/DayMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayMoneyApp.swift
+//  DayMoney
+//
+//  Created by 김태성 on 2023/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DayMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
