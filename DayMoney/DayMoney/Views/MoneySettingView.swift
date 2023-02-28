371b9e8ac47290ec05b0915cac2554b1ef4d4ce8
KTS224 <ts4683@gmail.com>
2023-02-28 20:17:05 +0900

    [Feat] 앱 사용중에 초기 돈을 변경가능 기능 구현

--- DayMoney/DayMoney/Views/MoneySettingView.swift
+++ DayMoney/DayMoney/Views/MoneySettingView.swift
@@ -14,22 +14,25 @@ struct MoneySettingView: View {
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        NavigationStack {
-            VStack {
-                TextField("용돈을 입력해 주세요.", text: $money.value)
-                
-                Button {
-                    UserDefaults.standard.set(Int(money.value), forKey: "총금액")
-                    moneyStore.money = UserDefaults.standard.integer(forKey: "총금액")
-                    UserDefaults.standard.set(false, forKey: "isShowingOnboardingView")
-                    isShowingOnboardingView = false
-                    dismiss()
-                } label: {
-                    Text("변경하기")
-                }
-                .disabled(money.value == "")
+        VStack {
+            TextField("용돈을 입력해 주세요.", text: $money.value)
+            
+            Button {
+                UserDefaults.standard.set(Int(money.value), forKey: "총금액")
+                moneyStore.money = UserDefaults.standard.integer(forKey: "총금액")
+                UserDefaults.standard.set(false, forKey: "isShowingOnboardingView")
+                isShowingOnboardingView = false
+                dismiss()
+            } label: {
+                Text("변경하기")
             }
-            .padding()
+            .disabled(money.value == "")
+        }
+        .padding()
+        .onDisappear {
+            /// 사용할 용돈을 변경했을경우 홈에 나오는 금액도 변경해주기 위한 로직
+            UserDefaults.standard.set(moneyStore.money / (Int(moneyStore.이달의마지막일)! - Int(moneyStore.오늘날)! + 1), forKey: "오늘사용가능한돈")
+            moneyStore.todayMoney = UserDefaults.standard.integer(forKey: "오늘사용가능한돈")
         }
     }
 }
